eaaa9089f1642d62aec98737af7fa0654c02b343
MiricleDev <rohan13k@outlook.com>
2025-02-16 00:22:51 +0000

    Created the chess board with all tiles ready

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Board from './board/Board';
+
+function App() {
+  return (
+    <div>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/board/BoardTile.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './boardTile.css'
+
+export const BoardTile = ({children, id, occupied, piece}) => {
+    const color = (children.charCodeAt(0) + Number(children[1])) % 2
+    return (
+        <div 
+            className={!color ? 'dark-tile' : 'light-tile'}
+            id={id}
+            >
+                {children}
+                
+        </div>
+    )
+}
